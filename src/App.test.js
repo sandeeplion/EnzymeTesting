d4bd5915e33a56b18719a2178fb9885410729903
sandeeplion <sandeepgandikota99@gmail.com>
2022-03-21 14:10:07 +0530

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,58 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme,{shallow} from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
+import Count from './count';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter:new EnzymeAdapter()});
+
+ /**
+  *factory Function to create a ShallowWrapper for the App Component.
+  * @function setup
+  * @returns {ShallowWrapper}
+  */
+
+const setup=()=>shallow(<Count />);
+
+const findByTestAttr=(wrapper,val)=>wrapper.find(`[data-test='${val}']`);
+
+test ('renders without error',()=>{
+
+  const wrapper=shallow(<App />);
+  const appComponent=findByTestAttr(wrapper,"component-app");
+  expect(appComponent.length).toBe(1);
+
+});
+
+test('renders increment button',()=>{
+  const wrapper=setup();
+  const button=findByTestAttr(wrapper,"increment-button");
+  expect(button.length).toBe(1);
+
+
+});
+
+test('renders counter display',()=>{
+
+  const wrapper=setup();
+  const counterDisplay=findByTestAttr(wrapper,"counter-display");
+  expect(counterDisplay.length).toBe(1);
+
+});
+
+test('counter display starts at 0',()=>{
+
+  const wrapper=setup();
+  const count=findByTestAttr(wrapper,"count").text();
+  expect(count).toBe("0");
+
+});
+test('clicking button increments counter display',()=>{
+
+  const wrapper=setup();
+  const button=findByTestAttr(wrapper,"increment-button");
+  button.simulate('click');
+  const count=findByTestAttr(wrapper,"count").text();
+  expect(count).toBe("1");
+
+
 });
---
+++ src/count.js
@@ -0,0 +1,22 @@
+import React, { useImperativeHandle, useState } from 'react';
+
+function Count(){
+
+    const [count,setCount]=useState(0);
+
+    function handleClick(){
+        return (
+        setCount(count+1)
+        )
+    }
+
+
+    return <div>
+        <h1>Welcome to testing page</h1>
+        <h4 data-test="counter-display">The count is <span data-test="count">{count}</span></h4>
+        <button data-test="increment-button"  onClick={handleClick} >Click to Increment</button>
+
+    </div>
+}
+
+export default Count;
